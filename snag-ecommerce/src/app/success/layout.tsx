8d5ce10553bdd4fc5d3662929272bdf96d2ebc59
Hamayun-khan <digiskill3d@gmail.com>
2025-12-16 07:08:24 +0000

    fix: disable all caching and fix prisma enum

--- snag-ecommerce/src/app/success/layout.tsx
+++ snag-ecommerce/src/app/success/layout.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+
 
 export default function SuccessLayout({
   children,
